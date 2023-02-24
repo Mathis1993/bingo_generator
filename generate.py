3e4e78acd8fcdee479d8d40ed43a6e6e2a955b01
Mathis Erichsen <erichsen@crossbow.de>
2023-02-24 20:13:55 +0100

    Add number argument

--- generate.py
+++ generate.py
@@ -1,9 +1,25 @@
+import argparse
+
 from generator import Generator
 
 
 def generate_bingo_pdf_files():
+    parser = argparse.ArgumentParser(
+        description="Bingo Generator generating html files."
+    )
+    parser.add_argument(
+        "-n",
+        "--number",
+        type=str,
+        help="Number of bingo files to produce."
+    )
+    args = parser.parse_args()
+    number = 10
+    if args.number:
+        number = int(args.number)
+
     g = Generator()
-    g.generate_samples()
+    g.generate_samples(number)
 
 
 if __name__ == '__main__':
