d1cdf90c3359a2d4324a6b6681b05f0eec0aa5f5
Mathis Erichsen <erichsen@crossbow.de>
2023-02-24 20:02:12 +0100

    Generate to html (for now)

--- generator.py
+++ generator.py
@@ -6,9 +6,8 @@ from typing import Dict, List
 
 import numpy as np
 import pandas as pd
-from weasyprint import CSS, HTML
-
 from exceptions import NotEnoughItemsException
+from weasyprint import CSS, HTML
 
 logger = logging.getLogger("generator")
 
@@ -26,7 +25,9 @@ class Generator:
         items_df = pd.read_excel(self.path_to_item_file)
         items_df = items_df.replace({np.nan: None})
         for items_column in items_df.columns:
-            items[items_column] = list(set([item for item in items_df[items_column].to_list() if item]))
+            items[items_column] = list(
+                set([item for item in items_df[items_column].to_list() if item])
+            )
 
         return items
 
@@ -40,7 +41,7 @@ class Generator:
 
     @cached_property
     def html(self) -> str:
-        with open(self.path_to_html_file, 'r') as f:
+        with open(self.path_to_html_file, "r") as f:
             return f.read()
 
     def generate_samples(self, number_of_samples: int = 100):
@@ -50,34 +51,44 @@ class Generator:
     def generate_one_sample(self):
         items_sample = self.get_items_for_one_sample()
         random.shuffle(items_sample)
-        items_df = self.create_df_from_items(items_sample)
-        items_html = items_df.to_html()
-        html_string = self.insert_items_into_html(items_html)
+
+        cells = [
+            f"""
+            <label>
+              <input type="checkbox">
+              <div class="mark"><span>{item}</span></div>
+            </label>"""
+            for item in items_sample
+        ]
+        cells = "".join(cell for cell in cells)
+
+        html_string = self.insert_items_into_html(cells)
         self.create_pdf_file(html_string)
 
     def get_items_for_one_sample(self) -> List[str]:
         items_sample = []
-        for number_of_items, items in zip(self.number_of_items_per_category, self.items.values()):
+        for number_of_items, items in zip(
+            self.number_of_items_per_category, self.items.values()
+        ):
             if number_of_items > (length := len(items)):
-                logger.error(f"Requested to sample {number_of_items} from item category with {length} items.")
+                logger.error(
+                    f"Requested to sample {number_of_items} from item category with {length} items."
+                )
                 raise NotEnoughItemsException
             items_sample.extend(random.sample(items, number_of_items))
         return items_sample
 
-    @staticmethod
-    def create_df_from_items(items: List[str]) -> pd.DataFrame:
-        dimension = int(np.sqrt(len(items)))
-        items_array = np.array(items).reshape(dimension, dimension)
-        return pd.DataFrame(items_array)
-
     def insert_items_into_html(self, items_html: str) -> str:
-        first_part, second_part = self.html.split("<body>")
-        return first_part + "<body>\n" + items_html + second_part
+        first_part, second_part = self.html.split("<small>")
+        return first_part + items_html + "<small>" + second_part
 
     def create_pdf_file(self, html_string: str):
         self.creation_counter += 1
-        html = HTML(string=html_string, base_url="./")
-        css = CSS(filename=self.path_to_css_file)
-        # necessary to provide if there is font configuration in the css file
-        # font_config = FontConfiguration()
-        html.write_pdf(target=f"bingo_{self.creation_counter}.pdf", stylesheets=[css]) #, font_config=font_config)
+        with open(f"bingo_{self.creation_counter}.html", 'w') as f:
+            f.write(html_string)
+        # weasyprint does somehow not respect the alignment of the cells
+        # html = HTML(string=html_string, base_url="./")
+        # css = CSS(filename=self.path_to_css_file)
+        # html.write_pdf(
+        #     target=f"bingo_{self.creation_counter}.pdf", stylesheets=[css]
+        # )
