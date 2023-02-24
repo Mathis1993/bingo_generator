51ad5b00381260f5d36547bd46cb064de1fce782
Mathis Erichsen <erichsen@crossbow.de>
2023-02-24 20:16:02 +0100

    Add path argument

--- generate.py
+++ generate.py
@@ -13,12 +13,21 @@ def generate_bingo_pdf_files():
         type=str,
         help="Number of bingo files to produce."
     )
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        help="Path to .xlsx file containing the items."
+    )
     args = parser.parse_args()
     number = 10
+    path = "./items.xlsx"
     if args.number:
         number = int(args.number)
+    if args.path:
+        path = args.path
 
-    g = Generator()
+    g = Generator(path_to_item_file=path)
     g.generate_samples(number)
 
 
